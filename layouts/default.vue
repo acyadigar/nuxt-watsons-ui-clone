3ffaa784e59c538402eb75032839b94f878da98f
acyadigar <alpercanyadigar@gmail.com>
2022-06-17 23:00:19 +0300

    Reorder the layout components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <TopBar />
     <Menu :activeBurgerMenu='activeBurgerMenu' @close='toggle' />
+    <TopBar />
     <Header @toggle='toggle'/>
       <Nuxt />
     <Footer />
